2c8878c1df60a45fbcb87636fbefacc763ede129
HHectorAlonso <Hector_alonso_martin@hotmail.es>
2025-05-15 11:44:11 +0000

    codigo finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  function generarAleatoria(){
+    let quien = who[Math.floor(Math.random()*who.length)];
+    let donde = action[Math.floor(Math.random()*action.length)];
+    let que = what[Math.floor(Math.random()*what.length)];
+    let cuando = when[Math.floor(Math.random()*when.length)];
+
+    return `${quien} ${donde} ${que} ${cuando}`;//
+  }
+  generarAleatoria();
+
+  document.getElementById("excuse").innerText = generarAleatoria();//
 };
